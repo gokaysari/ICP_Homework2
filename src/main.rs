6c5d0534dfb9907f535d980ba7cf4eb42bccd8e2
gokaysari <60223343+gokaysari@users.noreply.github.com>
2024-01-23 16:43:48 +0300

    second homework is done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io::{self, Write};
+
+pub struct WordCounter {
+    text: String,
+}
+
+impl WordCounter {
+    pub fn new(text: &str) -> Self {
+        WordCounter {
+            text: text.to_string(),
+        }
+    }
+
+    pub fn count_words(&self) -> usize {
+        self.text.split_whitespace().count()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter a text:");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let word_counter = WordCounter::new(&input);
+    let word_count = word_counter.count_words();
+
+    println!("Number of words: {}", word_count);
 }
